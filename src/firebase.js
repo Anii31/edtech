cf5aefd1c38bb95aa45e811db738c96a2c1bda1b
Aniket Prakash <aniket.prakash2020@gmail.com>
2025-05-31 00:42:22 +0530

    firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,16 +4,15 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDBfoPeO5MxuFK1aNva4e2IeyRv_lLL1AU",
-  authDomain: "loginsignup-d99d1.firebaseapp.com",
-  projectId: "loginsignup-d99d1",
-  storageBucket: "loginsignup-d99d1.firebasestorage.app",
-  messagingSenderId: "582164301893",
-  appId: "1:582164301893:web:e9224b61e9bd732be1fb2c",
-  measurementId: "G-F4H0FMQNSY"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
